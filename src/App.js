12596a0838b74cd286098fc3285f53885cd19f97
Rajayvardhan <rvsrathore2002@gmail.com>
2023-06-22 08:02:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Create state
+  state = {
+    xoffset: 550,
+    yoffset: 0,
+    delta: 10,
+  };
+
+  moveTitleToDown = () => {
+    this.setState(
+      {
+        yoffset: this.state.yoffset
+          + this.state.delta
+      });
+  };
+  moveTitleToRight = () => {
+    this.setState(
+      {
+        xoffset: this.state.xoffset
+          + this.state.delta
+      });
+  };
+  moveTitleToLeft = () => {
+    this.setState(
+      {
+        xoffset: this.state.xoffset
+          - this.state.delta
+      });
+  };
+  moveTitleToUp = () => {
+    this.setState(
+      {
+        yoffset: this.state.yoffset
+          - this.state.delta
+      });
+  };
+
+  render() {
+    return (
+      <div >
+        {/* Element to Move Dynamically */}
+        <div
+          style={{
+            position: "absolute",
+            left: `${this.state.xoffset}px`,
+            top: `${this.state.yoffset}px`,
+          }}
+          className="box">
+          move to this press the button
+        </div>
+
+        {/* Move Controls */}
+        <div className="container" >
+          <button onClick={this.moveTitleToRight} className="button-29" >
+            Move box To Right
+          </button>
+          <button onClick={this.moveTitleToDown} className="button-29">
+            Move box To Down
+          </button>
+          <button onClick={this.moveTitleToLeft} className="button-29">
+            Move box To Left
+          </button>
+          <button onClick={this.moveTitleToUp} className="button-29">
+            Move box To Up
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
